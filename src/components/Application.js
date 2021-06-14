3121bf029cd0445368396f6601e49630522b42e2
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-06-14 12:59:47 -0400

    Create interview list item and styles

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 
 export default function Application(props) {
+  const [day, setDay] = useState('Monday')
   const days = [
     {
       id: 1,
@@ -32,7 +33,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day="Monday" setDay={(day) => console.log(day)} />
+          <DayList days={days} day={day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
---
+++ src/components/InterviewListItem.js
@@ -0,0 +1,13 @@
+const InterviewListItem = (props) => {
+  return (
+    <li className="interviewers__item">
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/lpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+    </li>
+  )
+}
+
+export default InterviewListItem
