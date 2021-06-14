775b659df4985bb0957aeed8876fdc9330dee25a
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-06-14 14:22:31 -0400

    Add interviewer list stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewListItem from "components/InterviewListItem";
 
 //Avatar Image
 import avatarImage from '../public/images/joseph.png'
+import InterviewerList from "components/InterviewerList";
 
 //Button Stories
 storiesOf("Button", module)
@@ -104,5 +105,34 @@ storiesOf('InterviewerList', module)
       name={interviewer.name}
       avatar={interviewer.avatar}
       setInterviewer={action('setInterviewer')}
+    />
+  ))
+
+//Interview List Stories
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+storiesOf("InterviewList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+
+  .add('Initial', () => (
+    <InterviewerList
+      interviewers={interviewers}
+      setInterviewer={action('setInterviewer')}
+    />
+  ))
+
+  .add('Preselected', () => (
+    <InterviewerList
+      interviewers={interviewers}
+      interviewer={3}
+      setInterviewer={action("setInterviewer")}
     />
   ))
