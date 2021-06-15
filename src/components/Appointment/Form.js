29d4624c8824cae0991d0bb20d6ba848a98f71a1
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-06-15 14:29:03 -0400

    Add state to form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,27 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Button from 'components/Button'
 import InterviewerList from 'components/InterviewerList'
 
 const Form = ({ name, interviewers, interviewer, onSave, onCancel }) => {
-  const setInterviewer = {}
+  const [finalName, setFinalName] = useState(name || '')
+  const [finalInterviewer, setFinalInterviewer] = useState(interviewer || null)
+
+  const reset = () => {
+    setFinalName('')
+    setFinalInterviewer(null)
+  }
+
+  const cancel = () => {
+    reset()
+    onCancel()
+  }
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
+            value={finalName}
+            onChange={(e) => setFinalName(e.target.value)}
           />
         </form>
 
-        <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList interviewers={interviewers} value={interviewer} onChange={setFinalInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={onSave}>Save</Button>
         </section>
       </section>
