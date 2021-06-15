0fe5fad497e695d57ddb5d58bebeb6efc75acd66
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2021-06-15 16:23:10 -0400

    Add story for booked appointment

--- stories/index.js
+++ stories/index.js
@@ -183,3 +183,16 @@ storiesOf('Appointment', module)
       <Appointment id='last' time='1pm' />
     </Fragment>
   ))
+  .add('Appointment Booked', () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time='12pm'
+        interview={{ student: 'Lydia Miller-Jones', interviewer }}
+      />
+      <Appointment
+        id='last'
+        time='1pm'
+      />
+    </Fragment>
+  ))
